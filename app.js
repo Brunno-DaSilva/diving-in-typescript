f59c7e43dbc62c04ec34dfd2c7aaa3fd18a481da
Bruno DaSilva <hi.dasilvab@gmail.com>
2021-02-18 20:12:12 -0600

    Added css

--- app.js
+++ app.js
@@ -1,8 +1,12 @@
 var app = document.querySelector(".App");
 var addTwoNumbers = function (num1, num2) {
     var result = num1 + num2;
-    return "\n    <div class=\"App__info\">\n        <span id=\"numX\">" + num1 + " +</span>\n        <span id=\"numY\"> " + num2 + "</span>\n        <span id=\"result\"> = " + result + "</span>\n    </div>\n";
+    var html_data = " <div class=\"App__info\">\n  <span id=\"numX\">" + num1 + " +</span>\n  <span id=\"numY\"> " + num2 + " = </span>\n  <span id=\"result\">  " + result + "</span>\n</div>";
+    if (typeof num1 !== "number" || typeof num1 !== "number") {
+        throw new Error("Incorrect Input");
+    }
+    return html_data;
 };
 var numberOne = 3;
-var numberTwo = 5;
+var numberTwo = 2;
 app.innerHTML = addTwoNumbers(numberOne, numberTwo);
--- app.ts
+++ app.ts
@@ -3,16 +3,20 @@ const app = document.querySelector(".App");
 const addTwoNumbers = (num1: number, num2: number) => {
   let result = num1 + num2;
 
-  return `
-    <div class="App__info">
-        <span id="numX">${num1} +</span>
-        <span id="numY"> ${num2}</span>
-        <span id="result"> = ${result}</span>
-    </div>
-`;
+  const html_data = ` <div class="App__info">
+  <span id="numX">${num1} +</span>
+  <span id="numY"> ${num2} = </span>
+  <span id="result">  ${result}</span>
+</div>`;
+
+  //   if (typeof num1 !== "number" || typeof num1 !== "number") {
+  //     throw new Error("Incorrect Input");
+  //   }
+
+  return html_data;
 };
 
 const numberOne = 3;
-const numberTwo = 5;
+const numberTwo = 2;
 
 app.innerHTML = addTwoNumbers(numberOne, numberTwo);
